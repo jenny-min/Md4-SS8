8daed3beeb4ed575e520f17be671b9c753384521
jenny-min <jennynguyen.92.92@gmail.com>
2026-05-09 17:05:40 +0900

    ss8 first commit

---
+++ src/main/java/com/example/md4session8/Md4Session8Application.java
@@ -0,0 +1,13 @@
+package com.example.md4session8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4Session8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4Session8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md4session8/Md4Session8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md4session8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4Session8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
